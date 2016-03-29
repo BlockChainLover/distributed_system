6d16ab5ff7a9d780977a84c2e4d4a99e5f925aab
rulofg <sptg18@gmail.com>
2016-03-29 13:20:22 +0200

    added lsNode command

---
+++ src/commands/DfsLsNode.java
@@ -0,0 +1,26 @@
+package commands;
+
+public class DfsLsNode extends Command {
+
+	public DfsLsNode(String args[]){
+		setCommandId("dfs-lsNode");
+		String arguments[];
+		int ind = 0;
+		if(args.length > 0)
+			if(args[0].equals(getCommandId())){
+				ind = 1;
+			}
+		arguments = new String[args.length-ind];
+		for(int i = 0 ; i < arguments.length ; i++){
+			arguments[i] = args[i+ind];
+		}
+		this.setArgs(arguments);
+	}
+
+	@Override
+	public void action() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
